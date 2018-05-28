34676e10db35ecf87a77a72948a1662e3f89fb84
Naresh Madiraju <naresh.johnny@gmail.com>
2018-05-28 16:30:09 -0400

    emarket

--- build/web/app/app.py
+++ build/web/app/app.py
@@ -1,38 +1,15 @@
 # flask_web/app.py
 
-from flask import Flask
-from flask import Response
-from flask import request
-#from redis import Redis
-from datetime import datetime
-import MySQLdb
-import sys
-#import redis 
-import time
-import hashlib
-import os
-import json
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-dbconnection = MySQLdb.connect(host="192.168.0.13",
-                        user = "naresh",
-                        passwd = "P@ssword",
-                        db = "mydb")
-cursor = dbconnection.cursor()
-
-
  
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
-@app.route('/authors')
-def get_authors():
-        cursor.execute("select * from mydb.authors_tbl")
-        data = cursor.fetchone()
-        return data[0]
  
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=5000)
