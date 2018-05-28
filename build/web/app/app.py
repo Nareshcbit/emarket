535959273f4411cb5fcd57a54b4d5a3bab6723f8
Naresh Madiraju <naresh.johnny@gmail.com>
2018-05-28 17:13:25 -0400

    emarket

--- build/web/app/app.py
+++ build/web/app/app.py
@@ -1,6 +1,7 @@
 # flask_web/app.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 import yaml
 
 
@@ -14,13 +15,24 @@ app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 
 
- 
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/authors")
-	return render_template('authors.html')
- 
+def authors():
+    return render_template('authors.html')
+@app.route('/authors_add', methods=['GET', 'POST'])
+def authors_add():
+    if request.method == 'POST':
+        authorDetails = request.form
+        id = authorDetails['id']
+        firstname = authorDetails['firstname']
+        lastname = authorDetails['lastname']
+        return id
+    else:
+        return render_template('authors_add.html')
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=5000)
