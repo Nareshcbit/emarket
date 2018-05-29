5791d879e3d3d97363957ad9e59556c0b3f4341b
Naresh Madiraju <naresh.johnny@gmail.com>
2018-05-29 15:28:05 -0400

    emarket

--- build/db/populatedb.sh
+++ build/db/populatedb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- mysql -h 127.0.0.1 -u$MYSQL_USER -p$MYSQL_PASSWORD mydb< $MYSQL_SCRIPTS_PATH/initiatedb.sql
+ mysql -u$MYSQL_USER -p$MYSQL_PASSWORD mydb< $MYSQL_SCRIPTS_PATH/initiatedb.sql
