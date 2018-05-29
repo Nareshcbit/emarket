9b1bd10eb566f94a56487f289d13140980843528
Naresh Madiraju <naresh.johnny@gmail.com>
2018-05-29 17:44:45 -0400

    emarket

--- build/web/app/app.py
+++ build/web/app/app.py
@@ -12,10 +12,10 @@ app = Flask(__name__)
 
 # Configure db
 db = yaml.load(open('dbconf.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_HOST'] = db['MYSQL_HOST']
+app.config['MYSQL_USER'] = db['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = db['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = db['MYSQL_DB']
 
 
 mysql = MySQL(app)
