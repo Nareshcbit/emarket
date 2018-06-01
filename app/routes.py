33107edf198d4f611369a75207b2b3ae413e3b95
Naresh Madiraju <naresh.johnny@gmail.com>
2018-06-01 16:31:04 -0400

    removed all dirs

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, redirect
 from app import app
-from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -8,4 +7,3 @@ def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
 
-    
