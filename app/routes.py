f4099c5fe9c9d6a57ae3a05083d6eb2b86a70e94
Naresh Madiraju <naresh.johnny@gmail.com>
2018-06-04 13:21:38 -0400

    Bootstrap CSS

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,10 @@ from app import app
 def index():
     user = {'username': 'Naresh Madiraju'}
     parent_dict = [
-        {'A':'val1','B':'val2'},
-        {'C':'val3','D':'val4'}
+        {'uid':'1','vendor':'Dell', 'category':'Laptop', 'model':'XPS' ,'price':800},
+        {'uid':'2','vendor':'Apple', 'category':'Laptop', 'model':'Macbook Air' ,'price':1000},
+        {'uid':'3','vendor':'Apple', 'category':'Mobile', 'model':'iPhoneX' ,'price':1200},
+        {'uid':'4','vendor':'Samsung', 'category':'Mobile', 'model':'S9' ,'price':1100},
         ]
     return render_template('index.html', title='Home', user=user, parent_dict = parent_dict)
 
