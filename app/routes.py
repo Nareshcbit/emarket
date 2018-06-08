16d6eb8524af17a2f781f92a53dd48df833d873d
Naresh Madiraju <naresh.johnny@gmail.com>
2018-06-08 13:02:20 -0400

    Serialize Query Results

--- app/routes.py
+++ app/routes.py
@@ -53,10 +53,11 @@ def redis():
         key = search_category
         if (R_SERVER.get(key)):
             result_data = R_SERVER.get(key)
+            result_data = (result.data).decode('utf-8')
         else:
             matched_items = Items.query.filter_by(Category=search_category).all()
             result = items_schema.dump(matched_items)
-            result_data = result.data
+            result_data = (result.data).encode('utf-8')
             R_SERVER.set(key,result_data)
             R_SERVER.expire(key, 360)
 
