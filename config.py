7d78f86d5641a5d57eed906f445fd46ac3ae74da
Naresh Madiraju <naresh.johnny@gmail.com>
2018-06-11 10:52:09 -0400

    Access Redis Container

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REDIS_HOST = '192.168.0.18'
+    REDIS_HOST =  os.environ.get('REDIS_HOST') or '127.0.0.1'
